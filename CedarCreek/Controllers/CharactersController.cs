7607978653216bf0376bdb6435ec6c7c1a2877d8
Ry <thomashenrypaert@gmail.com>
2024-10-17 11:59:12 +0300

    Finished Index View, added CharacterController to make it work.

---
+++ CedarCreek/Controllers/CharactersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CedarCreek.Controllers
+{
+	public class CharactersController : Controller
+	{
+		//This controls all functions for characters, including missions.
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
