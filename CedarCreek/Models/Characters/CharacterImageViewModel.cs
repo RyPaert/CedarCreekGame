889214f1b89511e7933f7e99383942245f90e8aa
Opilane TTHK <opilane@tthk.ee>
2024-10-21 16:31:48 +0300

    Image
    Added model for images

---
+++ CedarCreek/Models/Characters/CharacterImageViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CedarCreek.Models.Characters
+{
+    public class CharacterImageViewModel
+    {
+        public Guid ImageID { get; set; }
+        public String ImageTitle { get; set; }
+        public byte[] ImageData { get; set; }
+        public string Image { get; set; }
+        public Guid? CharacterID { get; set; }
+
+    }
+}
