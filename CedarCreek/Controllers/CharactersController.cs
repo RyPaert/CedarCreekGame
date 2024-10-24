2b71d6c8af9990755b333cf20ded1ad0d9a7b974
Opilane TTHK <opilane@tthk.ee>
2024-10-24 14:15:11 +0300

    Controller Char

--- CedarCreek/Controllers/CharactersController.cs
+++ CedarCreek/Controllers/CharactersController.cs
@@ -3,6 +3,7 @@ using CedarCreek.Data;
 using CedarCreek.Models.Characters;
 using CedarCreek.Core.ServiceInterface;
 using CedarCreek.ApplicationServices.Services;
+using CedarCreek.Core.Dto;
 
 namespace CedarCreek.Controllers
 {
@@ -37,6 +38,39 @@ namespace CedarCreek.Controllers
 		{
 			CharacterCreateViewModel vm = new();
 			return View("Create",vm);
+		}
+		[HttpPost]
+		public async Task<IActionResult> Create(CharacterCreateViewModel vm)
+		{
+			var dto = new CharacterDto
+			{
+				CharacterName = vm.CharacterName,
+				CharacterHealth = 100,
+				CharacterXP = 0,
+				CharacterXPNextLevel = 100,
+				CharacterLevel = 0,
+				CharacterClass = (Core.Dto.CharacterClass)vm.CharacterClass,
+				CharacterStatus = (Core.Dto.CharacterStatus)vm.CharacterStatus,
+				PrimaryAttackName = vm.PrimaryAttackName,
+				PrimaryAttackPower = vm.PrimaryAttackPower,
+				SpecialAttackName = vm.SpecialAttackName,
+				SpecialAttackPower = vm.SpecialAttackPower,
+				CharacterCreationTime = vm.CharacterCreationTime,
+				Files = vm.Files,
+				Image = vm.Image.Select(x => new FileToDatabaseDto
+				{
+					ID = x.ImageID,
+					ImageData = x.ImageData,
+					ImageTitle = x.ImageTitle,
+					CharacterID = x.CharacterID,
+				}).ToArray(),
+            };
+			var result = await _charactersServices.Create(dto);
+
+			if (result != null)
+			{
+				return RedirectToAction("Index");
+			}
 		}
 	}
 }
