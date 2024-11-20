4a3ef346e9cf1232e866b285c62c30bcb4fd485e
Opilane TTHK <opilane@tthk.ee>
2024-11-20 15:04:56 +0200

    Added methods for AddPassowrd view, added the view itself too

---
+++ CedarCreek/Controllers/AccountsController.cs
@@ -0,0 +1,58 @@
+﻿using CedarCreek.Core.Domain;
+using CedarCreek.Core.Dto.AccountsDtos;
+using CedarCreek.Data;
+using CedarCreek.Models.Accounts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CedarCreek.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly CedarCreekContext _context;
+
+        public AccountsController
+            (UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager,
+            CedarCreekContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddPassword()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var userHasPassword = await _userManager.HasPasswordAsync(user);
+            if ( userHasPassword)
+            {
+                RedirectToAction("ChangePassword");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPassword(AddPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var result = await _userManager.AddPasswordAsync(user, model.NewPassword);
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+                await _signInManager.RefreshSignInAsync(user);
+                return View("AddPasswordConfirmation");
+            }
+            return View(model);
+        }
+    }
+}
