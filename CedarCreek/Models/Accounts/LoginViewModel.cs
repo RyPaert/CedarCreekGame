b7ace0021f766ef20175f653c26336804cbae092
Opilane TTHK <opilane@tthk.ee>
2024-11-20 14:10:26 +0200

    Register/Login
    Models > Accounts > ViewModels for Registering and Logging in

---
+++ CedarCreek/Models/Accounts/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CedarCreek.Models.Accounts
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Remember This Account?")]
+        public bool RememberMe { get; set; }
+        public string? returnURL { get; set; }
+   
+    }
+}
